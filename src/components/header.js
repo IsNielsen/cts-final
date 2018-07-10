aa1f7c5a4895e642a77346cb599ab5733ceabae0
CTS <isnielsen13@gmail.comgit config -- user.name isnielsen>
2018-07-10 10:53:19 -0600

    built first class compnent

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className="header">
+		<h1>Botega Mad Libs</h1>
+		<p> fill out the fields below to click and click the Generate button to see the map up story</p>
+		</div>
+	)
+}
+
+export default Header;
